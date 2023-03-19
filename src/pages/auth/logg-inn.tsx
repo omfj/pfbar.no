8061546297abaca599e1c7df62aeb1148eb1c05e
Ole Magnus Fon Johnsen <me@omfj.no>
2023-03-19 13:37:32 +0100

    🚸 Add Google provider

--- src/pages/auth/logg-inn.tsx
+++ src/pages/auth/logg-inn.tsx
@@ -1,9 +1,11 @@
-import Container from "@/components/container";
-import { getServerAuthSession } from "@/server/auth";
 import { type GetServerSideProps } from "next";
 import { type Provider } from "next-auth/providers";
 import { getProviders, signIn } from "next-auth/react";
 import Head from "next/head";
+
+import Container from "@/components/container";
+import { Button } from "@/components/ui/button";
+import { getServerAuthSession } from "@/server/auth";
 
 type Props = {
   providers: Array<Provider>;
@@ -19,23 +21,21 @@ const SignInPage: React.FC<Props> = ({ providers }) => {
         <h1 className="mb-3 text-center text-5xl font-bold">
           Velg en måte å logge inn på
         </h1>
-        <p className="mb-10 text-center text-xl">
+        <p className="mb-5 text-center text-xl">
           Du kan logge inn med en av de følgende tjenestene:
         </p>
-        <div className="my-10">
-          <ul className="mx-auto w-fit">
-            {Object.values(providers ?? []).map((provider) => (
-              <li key={provider.id}>
-                <button
-                  className="border border-black bg-black px-4 py-2 font-bold text-white transition-colors duration-150 hover:bg-white hover:text-black"
-                  onClick={() => void signIn(provider.id, { callbackUrl: "/" })}
-                >
-                  Logg inn med {provider.name}
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
+        <ul className="mx-auto my-10 flex w-fit flex-col justify-center gap-4">
+          {Object.values(providers ?? []).map((provider) => (
+            <li key={provider.id}>
+              <Button
+                onClick={() => void signIn(provider.id, { callbackUrl: "/" })}
+                fullWidth
+              >
+                Logg inn med {provider.name}
+              </Button>
+            </li>
+          ))}
+        </ul>
       </Container>
     </>
   );
