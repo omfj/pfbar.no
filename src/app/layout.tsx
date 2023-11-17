b7f5f838f051f2d316f2eed69dee4185ca57a094
Ole Magnus Fon Johnsen <me@omfj.no>
2023-11-17 14:47:16 +0100

    init new

---
+++ src/app/layout.tsx
@@ -0,0 +1,26 @@
+import "@/styles/globals.css";
+import { cn } from "@/utils/cn";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--inter-font",
+  weight: "500",
+});
+
+export const metadata: Metadata = {
+  title: "Pink Flamingo",
+};
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="no">
+      <body className={cn(inter.variable)}>{children}</body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,36 @@
+import type { Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+
+export default {
+  content: ["./src/**/*.{ts,js,tsx,jsx}"],
+  theme: {
+    fontFamily: {
+      primary: ["var(--inter-font)", ...fontFamily.sans],
+    },
+    extend: {
+      colors: {
+        foreground: "#000",
+        background: "#fff",
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [
+    require("tailwindcss-animate"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+  ],
+} satisfies Config;
