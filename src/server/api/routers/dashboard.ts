1b123488e11b227e5437c606a3f2081e05139e32
Ole Magnus Fon Johnsen <me@omfj.no>
2023-12-09 18:57:35 +0100

    remove unused

--- src/server/api/routers/dashboard.ts
+++
@@ -1,7 +0,0 @@
-import { createTRPCRouter, publicProcedure } from "../trpc";
-
-export const dashboardRouter = createTRPCRouter({
-  getUsers: publicProcedure.query(async ({ ctx }) => {
-    return await ctx.prisma.user.findMany();
-  }),
-});
